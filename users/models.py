f2def3c0fec3f90272f3cd25eb2c1b2288a70afe
princewill <princewillolaiya@gmail.com>
2021-01-31 11:21:43 +0100

    created custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    class Meta:
+        ordering = ("first_name")
